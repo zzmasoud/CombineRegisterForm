8b273494cef6ef6f1d7c9b9a5c52c195b009e1f1
Masoud Sh <zzmasoud@gmail.com>
2022-10-03 18:39:57 +0200

    Add UI components and connected `IBOutlet` vars.

--- CombineRegisterForm/View/ViewController.swift
+++ CombineRegisterForm/View/ViewController.swift
@@ -6,8 +6,23 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var usernameLoadingActivity: UIActivityIndicatorView!
+    @IBOutlet private weak var usernameErrorLabel: UILabel!
+
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var passwordErrorLabel: UILabel!
+
+    @IBOutlet private weak var passwordRepeatTextField: UITextField!
+    @IBOutlet private weak var passwordRepeatErrorLabel: UILabel!
+
+    @IBOutlet private weak var button: UIButton!
+    
+    private var subscriptions = [AnyCancellable]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
