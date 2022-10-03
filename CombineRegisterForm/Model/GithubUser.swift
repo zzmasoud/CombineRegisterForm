f9547d482ad95230a8a18638de78195631577845
Masoud Sh <zzmasoud@gmail.com>
2022-10-03 18:28:13 +0200

    Extend `GithubUser` to becomes `CustomStringConvertible`

--- CombineRegisterForm/Model/GithubUser.swift
+++ CombineRegisterForm/Model/GithubUser.swift
@@ -20,5 +20,19 @@ struct GithubUser: Decodable {
     let login: String
     let public_repos: Int
     let avatar_url: String
-    let created_at: String
+    private let created_at: String
+    
+    var createdAt: Date {
+        return Self.dateFormatter.date(from: created_at) ?? Date()
+    }
+}
+
+extension GithubUser: CustomStringConvertible {
+    var description: String {
+        "\n" +
+        "public repo(s): \(public_repos) \n" +
+        "avatar URL: \(avatar_url) \n" +
+        "created at: \(createdAt)"
+        
+    }
 }
