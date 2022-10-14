9dcad0b3df244aa0512d1017a2e22d80c5536642
Masoud Sh <zzmasoud@gmail.com>
2022-10-14 10:08:46 +0200

    Add new class `FormModel` to couple related codes

---
+++ CombineRegisterForm/Model/FormModel.swift
@@ -0,0 +1,13 @@
+//
+//  FormModel.swift
+//  CombineRegisterForm
+//
+//  Created by Masoud Sheikh Hosseini on 10/14/22.
+//
+
+import Foundation
+import Combine
+
+class FormModel: ObservableObject {
+    
+}
