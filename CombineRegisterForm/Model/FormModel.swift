214b5eb52c98650ab44dfb0ad8ab8a328e53bbb7
Masoud Sh <zzmasoud@gmail.com>
2022-10-15 12:29:30 +0200

    Prepare class by defining publishers and vars

--- CombineRegisterForm/Model/FormModel.swift
+++ CombineRegisterForm/Model/FormModel.swift
@@ -10,4 +10,22 @@ import Combine
 
 class FormModel: ObservableObject {
     
+    // MARK: Publishers
+    private let passwordPub = CurrentValueSubject<String, Never>("")
+    private let passwordRepeatPub = CurrentValueSubject<String, Never>("")
+    var canSubmit: AnyPublisher<Bool, Never>!
+
+    @Published var password = "" {
+        didSet {
+            passwordPub.send(password)
+        }
+    }
+    @Published var repeatPassword = "" {
+        didSet {
+            passwordRepeatPub.send(repeatPassword)
+        }
+    }
+    
+    private var subscriptions = [AnyCancellable]()
+    
 }
