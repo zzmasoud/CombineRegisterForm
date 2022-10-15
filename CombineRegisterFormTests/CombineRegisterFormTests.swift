5aef33822465c331e61934a128cf6028920d2dc2
Masoud Sh <zzmasoud@gmail.com>
2022-10-15 14:15:27 +0200

    Add password length validation

--- CombineRegisterFormTests/CombineRegisterFormTests.swift
+++ CombineRegisterFormTests/CombineRegisterFormTests.swift
@@ -6,8 +6,12 @@
 //
 
 import XCTest
+import Combine
+
 
 final class CombineRegisterFormTests: XCTestCase {
+    
+    var subscriptions = [AnyCancellable]()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,19 +21,20 @@ final class CombineRegisterFormTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func test_validatesPasswordLength() {
+        let expectation = XCTestExpectation(description: "waiting...")
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        let sut = FormModel()
+        sut.password = "12345678"
+        sut.repeatPassword = sut.password
+        
+        sut.$validationErrorMsg
+            .sink(receiveValue: { value in
+                XCTAssertNil(value)
+                expectation.fulfill()
+            })
+            .store(in: &subscriptions)
+        
+        wait(for: [expectation], timeout: 1)
     }
-
 }
